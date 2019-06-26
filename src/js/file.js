7cee05cd3671a7ad79bd2b3737ced48ff3ca23f7
Anja <rasoloanja@gmail.com>
2019-06-25 20:55:05 -0400

    Init

---
+++ src/js/file.js
@@ -0,0 +1,34 @@
+
+
+var a = new TimelineMax
+        a
+        .to('.sidebar__toggle--1', .4, {rotation: 135, position:'absolute', top: '1rem',ease: Power2.easeOut})
+        .to('.sidebar__toggle--3', .4, {rotation: -135, position:'absolute', top: '1rem', ease: Power2.easeOut},'-=.4')  
+        .to('.sidebar', .4, {x: '0'}, '-=.4')
+        .stop()
+
+var b = new TimelineMax
+        b
+     .to('.sidebar__toggle--2', .4, {opacity: 0, scale: 0})
+     .stop()
+
+ var show = false;
+
+$('.sidebar__toggle').on('click', function(){
+    
+    if(show === false) {
+        a.play();
+        b.play()
+        show = true;
+    } else {
+        a.reverse();
+        b.reverse();
+        show = false;
+    }
+   
+})
+
+
+
+
+
