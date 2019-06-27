16f1d1132f4c3150cb3f72fbfaae8c31418746bd
Anja <rasoloanja@gmail.com>
2019-06-26 20:20:27 -0400

    Added logic for active class management on scroll

--- src/js/file.js
+++ src/js/file.js
@@ -14,19 +14,43 @@ var b = new TimelineMax
 
  var show = false;
 
+
+
 $('.sidebar__toggle').on('click', function(){
     
     if(show === false) {
         a.play();
         b.play()
         show = true;
+            var c = new TimelineMax
+            c
+                .to('html', .4, {overflowY: 'hidden'});
+
+
+                $('.sidebar__list__item').on('click', function(){
+                    a.reverse();
+                    b.reverse();
+                    show = false;
+                    var d = new TimelineMax
+                        d
+                            .to('html', .4, {overflowY: 'scroll'})
+                            })
+                        
+        
+ 
     } else {
         a.reverse();
         b.reverse();
         show = false;
+        var c = new TimelineMax
+            c
+                .to('html', .4, {overflowY: 'scroll'})
     }
    
 })
+
+
+
 
 $(function() {
     $(".sidebar__list__item").click(function() {
@@ -34,10 +58,104 @@ $(function() {
        $(".sidebar__list li").removeClass("sidebar__active");
        // add class to the one we clicked
        $(this).addClass("sidebar__active");
+
+       
     });
  });
 
 
 
+ var writer = new TimelineMax({repeat:-1, repeatDelay: 2.58});
+ //set d
+ writer
+ .to('.home__title__writer', 1.5, {x: -372})
+ .to('.home__title__hide', 1.5, {x: 8},'-=1.5')
+ .to('.home__title', 1.5, {x: '20%'},'-=1.5')
+ .to('.home__title--2', .2, {opacity: 1})
+ .to('.home__title--1', .2, {opacity: 0})
+ .to('.home__title__writer', 1.5, {x: 0})
+ .to('.home__title__hide', 1.5, {x: '100%'},'-=1.5')
+ .to('.home__title', 1.5, {x: '0%'},'-=1.5')
+
+ .to('.home__title__writer', 1.5, {x: -372},'+=2')
+ .to('.home__title__hide', 1.5, {x: -372},'-=1.5')
+ .to('.home__title', 1.5, {x: '20%'},'-=1.5')
+
+ .to('.home__title--2', .2, {opacity: 0})
+ .to('.home__title--1', .2, {opacity: 1})
+ .to('.home__title__writer', 1.5, {x: 0})
+ .to('.home__title__hide', 1.5, {x: 0},'-=1.5')
+ .to('.home__title', 1.5, {x: '0%'},'-=1.5')
 
 
+
+
+
+
+
+
+
+
+
+ $(document).on('scroll', function() {    
+    var scroll = $(window).scrollTop();
+    var home = $('#home').position();
+    var homePos = home.top;
+    if(scroll > homePos ){
+    $(".sidebar__list li").removeClass("sidebar__active");
+    $(".sidebar__list li.sidebar__list__item--home").addClass("sidebar__active");
+    } 
+})
+
+
+
+
+
+$(document).on('scroll', function() {    
+    var scroll = $(window).scrollTop();
+    var about = $('#about').position();
+    var aboutPos = about.top;
+    if(scroll > (aboutPos - 200) ){
+    $(".sidebar__list li").removeClass("sidebar__active");
+    $(".sidebar__list li.sidebar__list__item--about").addClass("sidebar__active");
+    } 
+})
+
+$(document).on('scroll', function() { 
+    
+    var scroll = $(window).scrollTop();
+    var portfolio = $('#portfolio').position();
+    var portfolioPos = portfolio.top;
+    if(scroll > (portfolioPos - 300 )) {
+    $(".sidebar__list li").removeClass("sidebar__active");
+    $(".sidebar__list li.sidebar__list__item--portfolio").addClass("sidebar__active");
+    } 
+})
+
+$(document).on('scroll', function() { 
+    
+    var scroll = $(window).scrollTop();
+    var skills = $('#skills').position();
+    var skillsPos = skills.top; 
+    if(scroll > (skillsPos - 300) ){
+    $(".sidebar__list li").removeClass("sidebar__active");
+    $(".sidebar__list li.sidebar__list__item--skills").addClass("sidebar__active");
+    } 
+})
+
+
+$(document).on('scroll', function() { 
+    
+    var scroll = $(window).scrollTop();
+    var contact = $('#contact').position();
+    var contactPos = contact.top;
+    if(scroll > (contactPos - 300) ){
+    $(".sidebar__list li").removeClass("sidebar__active");
+    $(".sidebar__list li.sidebar__list__item--contact").addClass("sidebar__active");
+    } 
+})
+
+
+
+
+ 
